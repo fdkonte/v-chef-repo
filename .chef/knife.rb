3acbdea57665d6f81cbcc4cf832fcb60ab635081
Jack Konte <jack.konte@gmail.com>
2016-03-21 20:51:54 -0700

    added nodes

--- .chef/knife.rb
+++ .chef/knife.rb
@@ -9,3 +9,7 @@ validation_client_name   "desktopvm-validator"
 validation_key           "#{current_dir}/desktopvm-validator.pem"
 chef_server_url          "https://vagrant-chef.ad.desktopvm.com/organizations/desktopvm"
 cookbook_path            ["#{current_dir}/../cookbooks"]
+cookbook_copyright "DesktopVM Inc."
+cookbook_license "apachev2"
+cookbook_email "jack.konte@gmail.com"
+knife[:editor] = "C:\\Users\\Jack\\AppData\\Local\\atom\\Update.exe --processStart atom.exe"
---
+++ cookbooks/web-app/metadata.rb
@@ -0,0 +1,7 @@
+name             'web-app'
+maintainer       'DesktopVM Inc.'
+maintainer_email 'jack.konte@gmail.com'
+license          'Apache 2.0'
+description      'Installs/Configures web-app'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
